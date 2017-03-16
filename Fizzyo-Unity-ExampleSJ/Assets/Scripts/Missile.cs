2705cbf736e55b303e7ba57aa9bbbc48ca128024
Simon Jackson <darkside@zenithmoon.com>
2017-03-16 17:37:36 +0000

    New version of the Unity Example with fixes

---
+++ Fizzyo-Unity-ExampleSJ/Assets/Scripts/Missile.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class Missile : MonoBehaviour {
+
+    Vector3 dir = new Vector3(0.1f,0,0);
+    int age = 0;
+    int lifeSpan = 1000;
+	
+	// Update is called once per frame
+	void Update () {
+
+        var pos = transform.position;
+        pos += dir;
+        transform.position = pos;
+
+        if(age > lifeSpan)
+        {
+            Destroy(gameObject);
+        }
+        age++;
+    }
+}
